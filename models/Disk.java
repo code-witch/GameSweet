ec4bc16145e173a6ef56a88070a6c20c99b198a8
GeraldCacalda <47877944+GeraldCacalda@users.noreply.github.com>
2019-03-01 14:17:44 -0700

    Add files via upload

---
+++ models/Disk.java
@@ -0,0 +1,58 @@
+package gamesweet.Othello.models;
+
+public class Disk {
+	private String[] colors = {"black", "white"};
+	private String sideColor;
+	private boolean isSandwiched;
+
+	public Disk() {
+	}
+
+
+	public void flipDisk() {
+		if(isSandwiched()) {
+			if(sideColor.equals(colors[1])) {
+				setSideColor(colors[0]);
+			}else {
+				setSideColor(colors[1]);
+			}
+		}
+		
+	}
+	
+
+	public String getSideColor() {
+		return sideColor;
+	}
+
+	public void setSideColor(String sideColor) {
+		this.sideColor = sideColor;
+	}
+
+	public boolean isSandwiched() {
+		return isSandwiched;
+	}
+
+	public void setSandwiched(boolean isSandwiched) {
+		this.isSandwiched = isSandwiched;
+		flipDisk();
+	}
+
+	public String[] getColors() {
+		return colors;
+	}
+	
+	public String getColor(int index) {
+		return colors[index];
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Disk ").append(", sideColor=").append(sideColor);
+		return builder.toString();
+	}
+	
+
+}
