6b275416b677ef34214087c335d9106ce6b4c34c
CeceStoner <cstoner@student.neumont.edu>
2019-03-01 14:08:03 -0700

    Added Gui Class

--- gamesweet/connect4/application/Main.java
+++ gamesweet/connect4/application/Main.java
@@ -1,26 +1,17 @@
 package gamesweet.connect4.application;
 	
+import gamesweet.connect4.controllers.ConnectFour;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		new ConnectFour().init(primaryStage, "Player 1", "Player 2");
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public static void main(String[] arg) {
+		launch(arg);
 	}
 }
--- gamesweet/connect4/controllers/ConnectFour.java
+++ gamesweet/connect4/controllers/ConnectFour.java
@@ -1,66 +1,139 @@
 package gamesweet.connect4.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import gamesweet.connect4.models.*;
+import javafx.stage.Stage;
+import gamesweet.connect4.GUI.GUI;
+import gamesweet.connect4.enums.*;
 
 public class ConnectFour {
+	private String player1;
+	private String player2;
 	private Player[] players;
-	private int turnCount;
-	
-	public static void run() {
-		initializeGame();
-	}
-	
-	private static void initializeGame() {
+	private Board board;
+	private static int turnCount;
+
+	public void run() {
 		createPlayers();
+		createBoard(selectDifficulty());
+		playGame();
 	}
-	
-	private static void createPlayers() {
-		
+
+	public void init(Stage primaryStage, String name1, String name2) {
+		//Calls the GUI class
+		GUI.run(primaryStage);
+		player1 = name1;
+		player2 = name2;
+//		run();
 	}
-	
-	private static void addToLeaderboard() {
-		
+
+	private void createPlayers() {
+		// Prompt Player 1 for if they are on the leaderboard.
+			// 2 Options:
+			// Saved User
+			// Guest
+
+		// Prompt Player 1 for if they are on the leaderboard.
+			// 2 Options:
+			// Saved User
+			// Guest
+
+		// Hard-coded names for Guest
+		ArrayList<Chip> chips = new ArrayList<>();
+		for (int i = 0; i < 21; i++) {
+			Chip c = new Chip(ChipColor.BLACK);
+			chips.add(c);
+		}
+		Player black = new Player(player1, chips);
+		players[0] = black;
+
+		// Hard-coded names for Guest
+		ArrayList<Chip> chips0 = new ArrayList<>();
+		for (int i = 0; i < 21; i++) {
+			Chip c = new Chip(ChipColor.RED);
+			chips0.add(c);
+		}
+		Player red = new Player(player2, chips0);
+		players[0] = red;
 	}
-	
-	private static void playGame() {
-		
+
+	private void createBoard(int difficulty) {
+		Space[][] spaces;
+		if (difficulty == 1) {
+			// Easy
+			spaces = new Space[5][4];
+			board = new Board(spaces);
+		} else if (difficulty == 2) {
+			// Normal
+			spaces = new Space[7][6];
+			board = new Board(spaces);
+		} else if (difficulty == 3) {
+			// Challenging
+			spaces = new Space[9][8];
+			board = new Board(spaces);
+		}
 	}
-	
-	private static void printTurnState() {
-		
+
+	private void playGame() {
+		do {
+			if (turnCount % 2 == 0) {
+				takeTurn(players[0]);
+				turnCount++;
+			} else if (turnCount % 2 == 1) {
+				takeTurn(players[1]);
+				turnCount++;
+			}
+		} while (!checkForWin());
+		if (turnCount % 2 == 0) {
+			declareWinner(players[1]);
+		} else if (turnCount % 2 == 1) {
+			declareWinner(players[0]);
+		}
+
 	}
-	
-	private static void takeTurn(Player player) {
-		
+
+	// Method might be obsolete
+	private void printTurnState() {
+
 	}
-	
-	private static boolean checkForWin() {
+
+	private void takeTurn(Player player) {
+
+	}
+
+	private boolean checkForWin() {
 		return true;
 	}
-	
-	private static void declareWinner() {
-		
+
+	private void declareWinner(Player player) {
+		// This winner wins!!
 	}
-	
-	private static void resetGame() {
-		
+
+	private void resetGame() {
+		// Clear everything and restart game.
+		// Button at the top of the ribbon to be clicked anytime.
 	}
-	
-	private static void selectDifficulty() {
-		
+
+	private int selectDifficulty() {
+		// Logic to allow users to input difficulty
+		return 2;
 	}
-	
-	private static void createLeaderboard() {
-		
+
+	private void createLeaderboard() {
+
 	}
-	
-	private static void saveLeaderboard(HashMap<String, Player> leaderboard) {
-		
+
+	private void addToLeaderboard() {
+
 	}
-	
-	private static void printLeaderboard() {
-		
+
+	private void saveLeaderboard(HashMap<String, Player> leaderboard) {
+
+	}
+
+	private void printLeaderboard() {
+
 	}
 }
