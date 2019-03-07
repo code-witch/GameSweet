7a381520f405c1662ae429790c71419d3cdf7542
code-witch <>
2019-03-07 14:22:17 -0700

    hard coding is life

--- gamesweet/stratego/models/Player.java
+++ gamesweet/stratego/models/Player.java
@@ -29,19 +29,61 @@ public class Player {
 
 	public void initPieces() {
 		pieces[0][0] = new Character("Spy", 1, color, CharacterType.SPY);
-		pieces[0][1] = new Flag("Flag", 10, color);
+		pieces[0][1] = new Flag("Flag", 0, color);
+		pieces[0][2] = new Character("Marshal", 10, color, CharacterType.MARSHAL);
+		pieces[0][3] = new Character("General",9,color,null);
+		
+		pieces[0][4] = new Character("Colonel",8,color,CharacterType.COLONEL);
+		pieces[0][5] = new Character("Colonel",8,color,CharacterType.COLONEL);
+		
+		pieces[0][6] = new Character("Major",7,color,CharacterType.MAJOR);
+		pieces[0][7] = new Character("Major",7,color,CharacterType.MAJOR);
+		pieces[0][8] = new Character("Major",7,color,CharacterType.MAJOR);
+		
+		pieces[0][9] = new Character("Captain",6,color,CharacterType.CAPTAIN);
+		pieces[1][0] = new Character("Captain",6,color,CharacterType.CAPTAIN);
+		pieces[1][1] = new Character("Captain",6,color,CharacterType.CAPTAIN);
+		pieces[1][2] = new Character("Captain",6,color,CharacterType.CAPTAIN);
+
+		pieces[1][3] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
+		pieces[1][4] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
+		pieces[1][5] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
+		pieces[1][6] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
+		
+		pieces[1][7] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
+		pieces[1][8] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
+		pieces[1][9] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
+		pieces[2][0] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
+
+		pieces[2][1] = new Character("Miner",3,color,CharacterType.MINER);
+		pieces[2][2] = new Character("Miner",3,color,CharacterType.MINER);
+		pieces[2][3] = new Character("Miner",3,color,CharacterType.MINER);
+		pieces[2][4] = new Character("Miner",3,color,CharacterType.MINER);
+		pieces[2][5] = new Character("Miner",3,color,CharacterType.MINER);
+		
+		pieces[2][6] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[2][7] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[2][8] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[2][9] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[3][0] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[3][1] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[3][2] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[3][3] = new Character("Scout",2,color,CharacterType.SCOUT);
+		
+		pieces[3][4] = new Bomb("Bomb",100,color);
+		pieces[3][5] = new Bomb("Bomb",100,color);
+		pieces[3][6] = new Bomb("Bomb",100,color);
+		pieces[3][7] = new Bomb("Bomb",100,color);
+		pieces[3][8] = new Bomb("Bomb",100,color);
+		pieces[3][9] = new Bomb("Bomb",100,color);
+		
 	}
 
 //	Bomb (6x)
 //	Scout (8x)
-//	Miner (5x)
-//	Sergeant (4x)
-//	Lieutenant (4x)
-//	Captain (4x)
-//	Major (3x)
-//	Colonel (2x)
-//	General (1x)
-//	Marshall (1x)
+
+
+
 	
 	
 	public void setPieces(Piece[][] pieces) {
