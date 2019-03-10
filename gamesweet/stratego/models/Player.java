19bc5ea25bf663ec01a0b089302edb4d1d662d57
code-witch <>
2019-03-09 20:54:29 -0700

    Made names better

--- gamesweet/stratego/models/Player.java
+++ gamesweet/stratego/models/Player.java
@@ -29,47 +29,47 @@ public class Player {
 	}
 
 	public void initPieces() {
-		pieces[0][0] = new Character("Spy", 1, color, CharacterType.SPY);
+		pieces[0][0] = new Character("1", 1, color, CharacterType.SPY);
 		pieces[0][1] = new Flag("Flag", 0, color);
-		pieces[0][2] = new Character("Marshal", 10, color, CharacterType.MARSHAL);
-		pieces[0][3] = new Character("General",9,color,CharacterType.GENERAL);
+		pieces[0][2] = new Character("10", 10, color, CharacterType.MARSHAL);
+		pieces[0][3] = new Character("9",9,color,CharacterType.GENERAL);
 		
-		pieces[0][4] = new Character("Colonel",8,color,CharacterType.COLONEL);
-		pieces[0][5] = new Character("Colonel",8,color,CharacterType.COLONEL);
+		pieces[0][4] = new Character("9",8,color,CharacterType.COLONEL);
+		pieces[0][5] = new Character("9",8,color,CharacterType.COLONEL);
 		
-		pieces[0][6] = new Character("Major",7,color,CharacterType.MAJOR);
-		pieces[0][7] = new Character("Major",7,color,CharacterType.MAJOR);
-		pieces[0][8] = new Character("Major",7,color,CharacterType.MAJOR);
+		pieces[0][6] = new Character("7",7,color,CharacterType.MAJOR);
+		pieces[0][7] = new Character("7",7,color,CharacterType.MAJOR);
+		pieces[0][8] = new Character("7",7,color,CharacterType.MAJOR);
 		
-		pieces[0][9] = new Character("Captain",6,color,CharacterType.CAPTAIN);
-		pieces[1][0] = new Character("Captain",6,color,CharacterType.CAPTAIN);
-		pieces[1][1] = new Character("Captain",6,color,CharacterType.CAPTAIN);
-		pieces[1][2] = new Character("Captain",6,color,CharacterType.CAPTAIN);
+		pieces[0][9] = new Character("6",6,color,CharacterType.CAPTAIN);
+		pieces[1][0] = new Character("6",6,color,CharacterType.CAPTAIN);
+		pieces[1][1] = new Character("6",6,color,CharacterType.CAPTAIN);
+		pieces[1][2] = new Character("6",6,color,CharacterType.CAPTAIN);
 
-		pieces[1][3] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
-		pieces[1][4] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
-		pieces[1][5] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
-		pieces[1][6] = new Character("Lieutenant",5,color,CharacterType.LIEUTENANT);
+		pieces[1][3] = new Character("5",5,color,CharacterType.LIEUTENANT);
+		pieces[1][4] = new Character("5",5,color,CharacterType.LIEUTENANT);
+		pieces[1][5] = new Character("5",5,color,CharacterType.LIEUTENANT);
+		pieces[1][6] = new Character("5",5,color,CharacterType.LIEUTENANT);
 		
-		pieces[1][7] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
-		pieces[1][8] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
-		pieces[1][9] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
-		pieces[2][0] = new Character("Sergeant",4,color,CharacterType.SERGEANT);
+		pieces[1][7] = new Character("4",4,color,CharacterType.SERGEANT);
+		pieces[1][8] = new Character("4",4,color,CharacterType.SERGEANT);
+		pieces[1][9] = new Character("4",4,color,CharacterType.SERGEANT);
+		pieces[2][0] = new Character("4",4,color,CharacterType.SERGEANT);
 
-		pieces[2][1] = new Character("Miner",3,color,CharacterType.MINER);
-		pieces[2][2] = new Character("Miner",3,color,CharacterType.MINER);
-		pieces[2][3] = new Character("Miner",3,color,CharacterType.MINER);
-		pieces[2][4] = new Character("Miner",3,color,CharacterType.MINER);
-		pieces[2][5] = new Character("Miner",3,color,CharacterType.MINER);
+		pieces[2][1] = new Character("3",3,color,CharacterType.MINER);
+		pieces[2][2] = new Character("3",3,color,CharacterType.MINER);
+		pieces[2][3] = new Character("3",3,color,CharacterType.MINER);
+		pieces[2][4] = new Character("3",3,color,CharacterType.MINER);
+		pieces[2][5] = new Character("3",3,color,CharacterType.MINER);
 		
-		pieces[2][6] = new Character("Scout",2,color,CharacterType.SCOUT);
-		pieces[2][7] = new Character("Scout",2,color,CharacterType.SCOUT);
-		pieces[2][8] = new Character("Scout",2,color,CharacterType.SCOUT);
-		pieces[2][9] = new Character("Scout",2,color,CharacterType.SCOUT);
-		pieces[3][0] = new Character("Scout",2,color,CharacterType.SCOUT);
-		pieces[3][1] = new Character("Scout",2,color,CharacterType.SCOUT);
-		pieces[3][2] = new Character("Scout",2,color,CharacterType.SCOUT);
-		pieces[3][3] = new Character("Scout",2,color,CharacterType.SCOUT);
+		pieces[2][6] = new Character("2",2,color,CharacterType.SCOUT);
+		pieces[2][7] = new Character("2",2,color,CharacterType.SCOUT);
+		pieces[2][8] = new Character("2",2,color,CharacterType.SCOUT);
+		pieces[2][9] = new Character("2",2,color,CharacterType.SCOUT);
+		pieces[3][0] = new Character("2",2,color,CharacterType.SCOUT);
+		pieces[3][1] = new Character("2",2,color,CharacterType.SCOUT);
+		pieces[3][2] = new Character("2",2,color,CharacterType.SCOUT);
+		pieces[3][3] = new Character("2",2,color,CharacterType.SCOUT);
 		
 		pieces[3][4] = new Bomb("Bomb",100,color);
 		pieces[3][5] = new Bomb("Bomb",100,color);
